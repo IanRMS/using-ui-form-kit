f159f914f3f0a6bf4c7dcd71ab81b0a4cde4fb98
Ian Ribeiro <ianribeiro11@hotmail.com>
2020-05-20 10:38:57 -0300

    feat: adding components + package upgrade

--- src/App.js
+++ src/App.js
@@ -1,29 +1,48 @@
 import React, { useState } from "react";
-import { CheckBox, Divider, Input, SelectDropdown } from "react-ian-form-kit";
-import { Card, Container } from "./styles";
+import {
+  Button,
+  CheckBox,
+  Divider,
+  Input,
+  Modal,
+  Radio,
+  SelectDropdown,
+  Switch,
+  SwitchContainer,
+  SwitchInfo,
+} from "react-ian-form-kit";
+import { Card, Container, FlexContainer, InputContainer } from "./styles";
 
 function App() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
   const [job, setJob] = useState({ label: "Select...", value: 0 });
+  const [employee, setEmployee] = useState(false);
   const jobOptions = [
     { label: "Front-End", value: 1 },
     { label: "Back-End", value: 2 },
     { label: "Analista de Negócios", value: 3 },
     { label: "Full-Stack", value: 4 },
+    { label: "Estagiário", value: 5 },
   ];
   const skillList = [
-    { id: 1, label: "NodeJs" },
     { id: 2, label: "Angular" },
     { id: 3, label: "ReactJS" },
     { id: 4, label: "React-Native" },
     { id: 5, label: "Ionic" },
     { id: 6, label: "Flutter" },
-    { id: 7, label: "PHP" },
-    { id: 8, label: "Wordpress" },
   ];
-  const [skills, setSkills] = useState([skillList[1].id]);
+  const [skills, setSkills] = useState([skillList[0].id]);
+  const xpOptions = [
+    { id: 1, value: "1 year or less" },
+    { id: 2, value: "1 year - 2 years" },
+    { id: 3, value: "2 year - 4 years" },
+    { id: 4, value: "4 year or more" },
+  ];
+  const [xpTime, setTime] = useState(xpOptions[0].id);
 
   function selectJob(item) {
     setJob(item);
@@ -36,33 +55,49 @@ function App() {
         : [...skills, id]
     );
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen(true);
+    }, 3000);
+  }
   return (
     <Container>
       <Card>
         <h2>USING A UI FORM KIT</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Divider />
           <h3>Personal informations</h3>
-          <Input
-            type="text"
-            name="Name"
-            placeholder="Type your name"
-            value={name}
-            setValue={setName}
-            textColor="#fafafa"
-            color="#03a9f4"
-            floatingLabel={true}
-          />
-          <Input
-            type="email"
-            name="E-mail"
-            placeholder="Type your e-mail"
-            value={email}
-            setValue={setEmail}
-            textColor="#fafafa"
-            color="#03a9f4"
-            floatingLabel={true}
-          />
+          <FlexContainer>
+            <InputContainer>
+              <Input
+                type="text"
+                name="Name"
+                placeholder="Type your name"
+                value={name}
+                setValue={setName}
+                textColor="#fafafa"
+                color="#03a9f4"
+                floatingLabel={true}
+              />
+            </InputContainer>
+            <InputContainer>
+              <Input
+                type="email"
+                name="E-mail"
+                placeholder="Type your e-mail"
+                value={email}
+                setValue={setEmail}
+                textColor="#fafafa"
+                color="#03a9f4"
+                floatingLabel={true}
+              />
+            </InputContainer>
+          </FlexContainer>
           <Input
             type="password"
             name="Password"
@@ -75,7 +110,7 @@ function App() {
           />
           <Divider />
           <h3>Professional informations</h3>
-          <div style={{ width: "98%" }}>
+          <div style={{ width: "100%", maxWidth: 728, minWidth: 290 }}>
             <SelectDropdown
               name="Job"
               value={job.label}
@@ -85,19 +120,85 @@ function App() {
               floatingLabel={true}
             />
           </div>
-          {skillList.map((item) => (
-            <CheckBox
-              name="checkbox"
-              key={item.id}
-              value={item.label}
-              label={item.label}
-              checked={skills.includes(item.id)}
-              onChange={() => toggleSkill(item.id)}
-              color="#03a9f4"
-            />
-          ))}
+          <div style={{ width: "100%" }}>
+            <h4>- Select your skills</h4>
+            {skillList.map((item) => (
+              <CheckBox
+                name="checkbox"
+                key={item.id}
+                value={item.label}
+                label={item.label}
+                checked={skills.includes(item.id)}
+                onChange={() => toggleSkill(item.id)}
+                color="#03a9f4"
+              />
+            ))}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              flexWrap: "wrap",
+              justifyContent: "space-between",
+            }}
+          >
+            <div style={{ width: "45%" }}>
+              <h4>- Currently employed?</h4>
+              <SwitchContainer>
+                <Switch
+                  name="switch1"
+                  value={employee}
+                  checked={employee}
+                  onChange={() => setEmployee(!employee)}
+                  color="#03a9f4"
+                />
+                <SwitchInfo color="#fafafa">
+                  {employee ? "Yes" : "No"}
+                </SwitchInfo>
+              </SwitchContainer>
+            </div>
+            <div style={{ width: "45%" }}>
+              <h4>- Experience in job</h4>
+              {xpOptions.map((item) => (
+                <Radio
+                  name="radio"
+                  key={item.id}
+                  label={item.value}
+                  value={item.value}
+                  checked={xpTime === item.id}
+                  onChange={() => setTime(Number(item.id))}
+                  color="#03a9f4"
+                />
+              ))}
+            </div>
+          </div>
+          <Button
+            fullSize={true}
+            onLoading={loading}
+            loadingLabel="Carregando abestado..."
+            backgroundColor="#03a9f4"
+            type="submit"
+          >
+            Finish
+          </Button>
         </form>
       </Card>
+      <Modal
+        title="Modal Title"
+        open={open}
+        setOpen={setOpen}
+        closeOutsideClick={true}
+        message="A mensagem do bagulho vai aqui"
+      >
+        <Button
+          backgroundColor="#ff8f00"
+          fullSize={true}
+          onClick={() => setOpen(false)}
+          color="#424242"
+        >
+          Close modal
+        </Button>
+      </Modal>
     </Container>
   );
 }
--- src/styles.js
+++ src/styles.js
@@ -7,11 +7,12 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  padding-top: 80px;
 `;
 
 export const Card = styled.div`
   width: 90%;
-  max-width: 768px;
+  max-width: 500px;
   min-width: 320px;
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.4);
   padding: 20px;
@@ -26,5 +27,29 @@ export const Card = styled.div`
 
   h3 {
     color: #e0e0e0;
+    margin-bottom: 40px;
+  }
+
+  h4 {
+    color: #9e9e9e;
+    margin-bottom: 20px;
+  }
+`;
+
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+export const InputContainer = styled.div`
+  width: 48%;
+  min-width: 140px;
+  max-width: 260px;
+  /* margin: 0px 10px; */
+
+  @media (max-width: 554px) {
+    width: 100%;
   }
 `;
